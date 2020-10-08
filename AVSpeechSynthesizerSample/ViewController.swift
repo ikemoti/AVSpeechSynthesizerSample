783b355bef2344be50f48eaf7221ca0de36020f9
ikemoti <58681457+ikemoti@users.noreply.github.com>
2020-10-08 22:09:29 +0900

    読み上げ成功

--- AVSpeechSynthesizerSample/ViewController.swift
+++ AVSpeechSynthesizerSample/ViewController.swift
@@ -6,9 +6,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+   
+    @IBAction func tapButton(_ sender: Any) {
+        let synthesizer = AVSpeechSynthesizer()
+        let utterance = AVSpeechUtterance.init(string: "読み上げる文字列")
+        let voice = AVSpeechSynthesisVoice.init(language: "ja-JP")
+        utterance.voice = voice
+        synthesizer.speak(utterance)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
